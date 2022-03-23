e9ade01ba7fd9b126aa3370bebb70f697868530f
WescleyTorres <jewescley@gmail.com>
2022-03-23 13:50:34 -0300

    first commit

--- aula-keycloak/auth-back-keycloak/Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:14.15.4-alpine3.12
-
-RUN apk add --no-cache bash
-
-RUN npm install -g @nestjs/cli@8.0.0
-
-USER node
-
-WORKDIR /home/node/app
--- aula-keycloak/auth-front-keycloak/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import { useAuthHttp } from "../hooks/useAuthHttp";
-
-const Home: NextPage = () => {
-  const { data: user, error } = useAuthHttp("user");
-  return user ? <div>Hello World</div> : null;
-};
-
-export default Home;
